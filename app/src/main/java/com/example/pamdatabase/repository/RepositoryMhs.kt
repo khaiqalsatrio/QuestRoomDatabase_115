ff8a3571d05693c310a4a71dd6ae4ef23705cf37
Khaiqol <Khaiqol@gmail.com>
2024-12-15 12:55:35 +0700

    Room mencocokkan data yang dikirim melalui parameter mahasiswa dengan Primary Key di dalam tabel mahasiswa Jika Primary Key cocok, data tersebut akan dihapus. dan Room mencocokkan data berdasarkan Primary Key dari objek mahasiswa.     Jika Primary Key cocok, data di tabel akan diperbarui dengan nilai baru yang ada dalam parameter mahasiswa.

--- app/src/main/java/com/example/pamdatabase/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pamdatabase/repository/RepositoryMhs.kt
@@ -11,4 +11,9 @@ interface RepositoryMhs {
 
     fun getMhs(nim: String): Flow<Mahasiswa>
 
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa:Mahasiswa)
+
+
 }
