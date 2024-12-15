82a8701104324ff4a91de843d15b1dec19883028
Khaiqol <Khaiqol@gmail.com>
2024-12-15 13:20:07 +0700

    Kode ini adalah implementasi HomeMhsViewModel menggunakan MVVM dan StateFlow untuk mengelola serta menyajikan data mahasiswa dari Repository ke UI secara real-time.

--- app/src/main/java/com/example/pamdatabase/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pamdatabase/ui/viewmodel/HomeMhsViewModel.kt
@@ -2,9 +2,17 @@ package com.example.pamdatabase.ui.viewmodel
 
 import androidx.compose.runtime.State
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pamdatabase.Data.entity.Mahasiswa
 import com.example.pamdatabase.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMhsViewModel (
     private val repositoryMhs: RepositoryMhs
@@ -17,4 +25,31 @@ class HomeMhsViewModel (
                 isLoading = false,
             )
         }
-}
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+    }
+
+data class HomeUiState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
