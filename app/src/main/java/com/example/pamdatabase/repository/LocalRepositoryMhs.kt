ed80d0fce74621a796b36fc864329b5bf4c4268e
Khaiqol <Khaiqol@gmail.com>
2024-12-15 12:59:22 +0700

    Mengambil semua data mahasiswa dengan urutan berdasarkan nama secara ascending (ASC). Menggunakan Flow agar data mahasiswa yang dicari dapat dipantau secara real-time. Fungsi ini bersifat asynchronous dan dijalankan di background thread agar tidak memblokir main thread. Fungsi ini bersifat asynchronous dan dijalankan di background thread oleh Room.

--- app/src/main/java/com/example/pamdatabase/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pamdatabase/repository/LocalRepositoryMhs.kt
@@ -13,4 +13,20 @@ class LocalRepositoryMhs(
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
 }
