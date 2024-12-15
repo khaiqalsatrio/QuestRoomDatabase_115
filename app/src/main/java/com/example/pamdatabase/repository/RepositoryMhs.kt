ff24789440b250c8528fe7e1fb0283196e48134c
Khaiqol <Khaiqol@gmail.com>
2024-12-15 12:52:37 +0700

    Room akan menjalankan query SELECT * FROM mahasiswa di background thread. dan Room akan menjalankan query SELECT * FROM mahasiswa WHERE nim = :nim.

--- app/src/main/java/com/example/pamdatabase/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pamdatabase/repository/RepositoryMhs.kt
@@ -6,5 +6,9 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoryMhs {
 
     suspend fun insertMhs(mahasiswa: Mahasiswa)
-    
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
 }
