0f9606f1264486664c0a94ec4acc7099b892b508
Khaiqol <Khaiqol@gmail.com>
2024-12-15 13:52:38 +0700

    mendefinisikan tiga destinasi (home, detail, update) untuk navigasi dalam aplikasi. Dua destinasi (DestinasiDetail dan DetinasiUpdate) memerlukan parameter NIM, yang dimasukkan dalam route sebagai bagian dari URL. Objek ini digunakan untuk mempermudah manajemen dan penggunaan route dengan argumen di dalam aplikasi.

--- app/src/main/java/com/example/pamdatabase/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pamdatabase/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,19 @@ package com.example.pamdatabase.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val NIM = "nim"
+    val routeWithArg = "$route/{$NIM}"
+}
+
+object DetinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
